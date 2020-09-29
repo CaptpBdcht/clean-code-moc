f3bfcee032dcc7ebc8ee06d80d0a35229adabeb7
Julien GUILLAN <guillan.julien@live.com>
2020-09-29 15:03:34 +0200

    Partie 2

--- index.ts
+++ index.ts
@@ -1,11 +1,58 @@
 var stdin = process.openStdin();
 
-function fizzbuzz(value: number): String {
+interface FizzBuzzValues{
+    fizz?: number
+    buzz?: number
+    value?: number
+}
+
+console.log('Specify a value to fizz: ');
+stdin.addListener("data", function(inputValues) {
+    let inputValuesString = inputValues.toString();
+    let fizzBuzzValues = extractValuesFromInput(inputValuesString);
+    if(!checkData(fizzBuzzValues)){
+        return;
+    } else {
+        console.log(startFizzbuzz(fizzBuzzValues));
+    }
+});
+
+function checkData(values: FizzBuzzValues): Boolean{
+    if(values.fizz == 1 || values.buzz == 1){
+        console.log("Divisor cannot be 1");
+        return false;
+    } else if (values.fizz < 0 || values.buzz < 0){
+        console.log("Divisor cannot be negative");
+        return false;
+    } else if (values.fizz == values.buzz){
+        console.log("Divisor must be distinct");
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function extractValuesFromInput(inputData: String): FizzBuzzValues{
+    let fizzBuzzValues: FizzBuzzValues = {};
+    if(inputData.indexOf(',') !== -1){
+        let fizz = Number(inputData[inputData.indexOf(',') - 1]);
+        let buzz = Number(inputData[inputData.indexOf(',') + 1]);
+        let value = Number(inputData[inputData.indexOf(' ') + 1]);
+
+        fizzBuzzValues.fizz = fizz;
+        fizzBuzzValues.buzz = buzz;
+        fizzBuzzValues.value = value;
+    }
+    console.log(fizzBuzzValues);
+    return fizzBuzzValues;
+}
+
+function startFizzbuzz(data: FizzBuzzValues): String {
     let resultString: String = "";
-    for(let i: number = 1; i <= value; i++){
-        if(i % 3 == 0){
+    for(let i: number = 1; i <= data.value; i++){
+        if(i % data.fizz == 0){
             resultString += " fizz";
-        } else if (i % 5 == 0){
+        } else if (i % data.buzz == 0){
             resultString += " buzz";
         } else {
             resultString += " " + i.toString();
@@ -13,16 +60,3 @@ function fizzbuzz(value: number): String {
     }
     return resultString;
 }
-
-function startFizzbuzz(value: number){
-    if(value < 0){
-        console.log("Cannot fizzbuzz negative numbers");
-    } else {
-        console.log(fizzbuzz(value));
-    }
-}
-
-console.log('Specify a value to fizz: ');
-stdin.addListener("data", function(inputValue) {
-    startFizzbuzz(inputValue);
-});
