c48eeea4dd46b1f9aaa145a98374a0f58df41a36
vidal77390 <leo.vidal@wanadoo.fr>
2020-09-29 15:05:26 +0200

    part 2

--- Tp.ts
+++ Tp.ts
@@ -1,28 +1,48 @@
 
 
-function FizzBuzzTool(value: number){
-    if(!value){
-        printError("Please specify a value to fizzbuzz");
-        return;
-    }else if(value <= 0){
-        printError("Cannot fizzbuzz negative numbers");
-        return;
-    }
-    var contentToPrint: String = getContentToPrint(value);
+function FizzBuzzTool(valueForFizz:number, valueForBuzz: number, value: number){
+    if(checkErrorValue(value)) return;
+    if(checkErrorRules(valueForBuzz, valueForFizz, value)) return;
+    var contentToPrint: String = getContentToPrint(valueForFizz, valueForBuzz, value);
     console.log(contentToPrint);
 }
 
-function getContentToPrint(value : number){
+function checkErrorValue(value: number){
+    if(!value){
+        printError("Please specify a value to fizzbuzz");
+        return true;
+    }else if(value <= 0){
+        printError("Cannot fizzbuzz negative numbers");
+        return true;
+    }
+    else return false;
+}
+
+function checkErrorRules(divisor1: number, divisor2: number, value: number){
+    if(divisor1 === divisor2){
+        printError("Divisors must be distinct");
+        return true;
+    }else if(divisor1 <= 0 || divisor2 <= 0){
+        printError("Divisor cannot be negative");
+        return true;
+    }else if(divisor2>value || divisor1>value){
+        printError("Divisor must be a prime");
+        return true;
+    }
+    else return false;
+}
+
+function getContentToPrint(divisor1: number, divisor2: number, value : number){
     var content: String = "";
     for(let currentValue = 1; currentValue <= value; currentValue++){
-        content = content + contentToPrintForNumber(currentValue) + " ";
+        content = content + contentToPrintForNumber(divisor1, divisor2, currentValue) + " ";
     }
     return content;
 }
 
-function contentToPrintForNumber(value: number){
-    if(value%3 === 0) return "Fizz";
-    else if(value%5 === 0) return "Buzz";
+function contentToPrintForNumber(divisor1: number, divisor2: number, value: number){
+    if(value%divisor1 === 0) return "Fizz";
+    else if(value%divisor2 === 0) return "Buzz";
     else return value.toString();
 }
 
@@ -30,9 +50,11 @@ function printError(errorMessage: String){
     console.log(errorMessage);
 }
 
-console.log("fizzbuzz -5 (should error) : ");
-FizzBuzzTool(-5);
-console.log("fizzbuzz 5 (should: 1 2 Fizz 4 Buzz) : ");
-FizzBuzzTool(5);
-console.log("fizzbuzz 15 (should: 1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 Fizz) : ");
-FizzBuzzTool(15);
+console.log("fizzbuzz 3,5 -5 (should error) : ");
+FizzBuzzTool(3, 5, -5);
+console.log("fizzbuzz -3,5 5 (should error) : ");
+FizzBuzzTool(-3, 5, 5);
+console.log("fizzbuzz 3,5 5 (should: 1 2 Fizz 4 Buzz) : ");
+FizzBuzzTool(3, 5, 5);
+console.log("fizzbuzz 4,6 15 (should: 1 2 3 Fizz 5 Buzz 7 Fizz 9 10 11 Fizz 13 14 15 ) : ");
+FizzBuzzTool(4, 6, 15);
