2ad4cb4ef591946951a319138a4787960499ab32
Vincent Milano <vincent@kerala.vc>
2020-09-30 09:00:30 +0200

    P2 nearly full

--- FizzBuzz/src/fizzbuzz.spec.ts
+++ FizzBuzz/src/fizzbuzz.spec.ts
@@ -11,22 +11,54 @@ describe('FizzBuzz should', () => {
         fizzbuzz = new FizzBuzz();
     });
 
-    it('on range 3', () => {
-        fizzbuzz.range(3).should.equal(
-            '1 2 Fizz'
-        );
+    describe('when no divisors are given', () => {
+
+        it('generate range until 3', () => {
+            fizzbuzz.range(3).should.equal(
+                '1 2 Fizz'
+            );
+        });
+    
+        it('generate range until 15', () => {
+            fizzbuzz.range(15).should.equal(
+                '1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz'
+            );
+        });
+    
+        it('throw with negative numbers', () => {
+            (function () {
+                fizzbuzz.range(-42);
+            })
+            .should.throw('Cannot fizzbuzz negative numbers');
+        });
     });
 
-    it('on range 15', () => {
-        fizzbuzz.range(15).should.equal(
-            '1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz'
-        );
-    });
+    describe('with divisors', () => {
+        it('throw when they are the same', () => {
+            (function () {
+                fizzbuzz.withDivisors(2, 2)
+            }).should.throw('Divisors must be distinct');
+        });
 
-    it('should throw with negative numbers', () => {
-        (function () {
-            fizzbuzz.range(-42);
-        })
-        .should.throw('Cannot fizzbuzz negative numbers');
+        it('throw with ones', () => {
+            (function () {
+                fizzbuzz.withDivisors(1, 2).range(3);
+            })
+            .should.throw('Divisors cannot be 1');
+        });
+
+        it('throw with negative divisors', () => {
+            (function () {
+                fizzbuzz.withDivisors(-2, 5).range(3);
+            })
+            .should.throw('Divisors cannot be negative');
+        });
+
+        it('should throw with not prime values', () => {
+            (function () {
+                fizzbuzz.withDivisors(2, 4).range(3);
+            })
+            .should.throw('Divisors must be primes');
+        });
     });
 });
--- FizzBuzz/src/fizzbuzz.ts
+++ FizzBuzz/src/fizzbuzz.ts
@@ -1,16 +1,36 @@
 export class FizzBuzzError extends Error {
-    constructor() {
-        super('Cannot fizzbuzz negative numbers')
+    constructor(message: string) {
+        super(message)
     }
 }
 
 export class FizzBuzz {
+    private DIVISORS = {
+        Fizz: 3, Buzz: 5
+    }
+
+    readonly PRIMES_TO_100 = [
+        2, 3, 5, 7, 11,
+        13, 17, 19, 23, 29,
+        31, 37, 41, 43, 47,
+        53, 59, 61, 67, 71,
+        73, 79, 83, 89, 97
+    ];
+
     range(max: number): string {
         this.handleRangeErrors(max);
 
         return this.arrayOnRange(max)
                     .map(val => this.of(val))
                     .join(' ')
+    }
+
+    withDivisors(fizzDivisor: number, buzzDivisor: number): FizzBuzz {
+        this.handleDivisorsErrors(fizzDivisor, buzzDivisor);
+
+        this.DIVISORS.Fizz = fizzDivisor;
+        this.DIVISORS.Buzz = buzzDivisor;
+        return this;
     }
 
     private of(value: number): string {
@@ -45,7 +65,26 @@ export class FizzBuzz {
 
     private handleRangeErrors(max: number): void {
         if (max < 0) {
-            throw new FizzBuzzError();
+            throw new FizzBuzzError('Cannot fizzbuzz negative numbers');
         }
+    }
+
+    private handleDivisorsErrors(first: number, second: number): void {
+        if (first === second) {
+            throw new FizzBuzzError('Divisors must be distinct');
+        }
+        if (first === 1 || second === 1) {
+            throw new FizzBuzzError('Divisors cannot be 1');
+        }
+        if (first < 0 || second < 0) {
+            throw new FizzBuzzError('Divisors cannot be negative');
+        }
+        if (!this.isPrimeUpTo100(first) || !this.isPrimeUpTo100(second)) {
+            throw new FizzBuzzError('Divisors must be primes');
+        }
+    }
+
+    private isPrimeUpTo100(value: number): boolean {
+        return this.PRIMES_TO_100.indexOf(value) < 0 
     }
 }
