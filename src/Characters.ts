5949d51f073a55ac3d06cfb28ec0fe5f53815369
dmytrolutsyk <dmytro@LAPTOP-R5T7TI5S.localdomain>
2020-10-02 15:23:34 +0200

    first commit

---
+++ src/Characters.ts
@@ -0,0 +1,14 @@
+export abstract class Characters {
+
+    health: number;
+    name: String;
+    lifeStatus: String;
+
+    constructor() {
+        this.health = 100;
+        this.name = name;
+        this.lifeStatus = "alive";
+    }
+
+
+}
