87e342151db7fd20d711f39af10cccc6d8d63cf6
Julien GUILLAN <guillan.julien@live.com>
2020-11-09 23:05:00 +0100

    Entity test class

--- src/test/EntityTest.java
+++ src/test/EntityTest.java
@@ -1,2 +1,34 @@
-package test;public class EntityTest {
+package test;
+import main.Entity;
+import main.Priest;
+import main.Warrior;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class EntityTest {
+
+    static final int OAKHEALTH = 300;
+    Warrior floki = new Warrior("Floki");
+    Priest athelstan = new Priest("Athlestan");
+    Entity oak = new Entity(OAKHEALTH) {
+        @Override
+        public void die() {
+            //nothing to do here
+        }
+    };
+
+    @Test
+    public void shouldStartToCutDownTheOakToMakeBoats(){
+        int oakHealth = oak.getHealth();
+        floki.attack(oak);
+        assertTrue(oak.getHealth() < oakHealth);
+    }
+
+    @Test
+    public void shouldBeCuttedDown(){
+        while(oak.getHealth() > 0){
+            floki.attack(oak);
+        }
+        assertEquals(oak.getState(), Entity.states.DEAD);
+    }
 }
