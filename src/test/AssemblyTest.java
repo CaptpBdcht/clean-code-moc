99d364e05d07aa7bd2d7712b1d7c160b7188c87d
Ju <blackack91@gmail.copm>
2020-11-12 22:31:01 +0100

    Test class for assemblies

--- src/test/AssemblyTest.java
+++ src/test/AssemblyTest.java
@@ -3,6 +3,7 @@ package test;
 import main.Assembly;
 import main.Priest;
 import main.Warrior;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -16,6 +17,9 @@ public class AssemblyTest extends DataTest{
     ArrayList<Class<?>> allowedRoles = new ArrayList<Class<?>>(Collections.singletonList(Warrior.class));
     Assembly scandinavians = new Assembly("Scandinavians", allowedRoles);
 
+    ArrayList<Class<?>> enAllowedRoles = new ArrayList<>(Collections.singletonList(Warrior.class));
+    Assembly english = new Assembly("English", enAllowedRoles);
+
     @Test
     public void shouldHaveTorsteinAsMaster(){
         torstein.joinAssembly(scandinavians);
@@ -24,8 +28,45 @@ public class AssemblyTest extends DataTest{
     }
 
     @Test
+    public void shouldElectANewMaster(){
+        torstein.joinAssembly(scandinavians);
+        porunn.joinAssembly(scandinavians);
+        ragnar.joinAssembly(scandinavians);
+        floki.joinAssembly(scandinavians);
+        torstein.leaveAssembly();
+        assertNotEquals(scandinavians.getMaster(), torstein);
+        assertNotNull(scandinavians.getMaster());
+        System.out.println(scandinavians.getMaster().getName());
+    }
+
+    @Test
+    public void shouldMasterBeAllowedToUpdate(){
+        porunn.joinAssembly(scandinavians);
+        ragnar.joinAssembly(scandinavians);
+        floki.joinAssembly(scandinavians);
+        floki.updateAssemblyName("Vikings");
+        assertEquals(scandinavians.getName(), "Scandinavians");
+    }
+
+    @Test
+    public void shouldBeAbandonned(){
+        porunn.joinAssembly(scandinavians);
+        ragnar.joinAssembly(scandinavians);
+        ragnar.leaveAssembly();
+        porunn.leaveAssembly();
+        assertNull(scandinavians.getMaster());
+    }
+
+    @Test
     public void shouldOnlyAcceptWarriors(){
         athelstan.joinAssembly(scandinavians);
         assertNull(athelstan.getAssembly());
+    }
+
+    @Test
+    public void shouldChangeAssembly(){
+        porunn.joinAssembly(english);
+        porunn.joinAssembly(scandinavians);
+        assertEquals(porunn.getAssembly(), scandinavians);
     }
 }
